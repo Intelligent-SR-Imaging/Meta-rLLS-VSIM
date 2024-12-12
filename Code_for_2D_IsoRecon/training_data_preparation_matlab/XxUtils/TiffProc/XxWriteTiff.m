1076eacd0d34ebdd117c29d1d122cabb579274d6
Intelligent-Imaging <lidonglab@gmail.com>
2024-12-12 19:14:03 +0800

    Delete Code_for_2D_IsoRecon/training_data_preparation_matlab directory

--- Code_for_2D_IsoRecon/training_data_preparation_matlab/XxUtils/TiffProc/XxWriteTiff.m
+++
@@ -1,7 +0,0 @@
-function XxWriteTiff(data, FileName)
-
-imwrite(data(:,:,1), FileName, 'Compression','none');
-
-for i=2:size(data,3)
-    imwrite(data(:,:,i), FileName, 'WriteMode', 'append', 'Compression','none');
-end
